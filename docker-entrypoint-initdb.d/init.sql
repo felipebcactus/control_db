24349f37f93dd6d9f9def1129f5fb46c322679ce
felipe bevi <felipe@9ideias.com>
2024-05-09 15:23:53 -0300

    alterado pra usar DB MYSQL

--- docker-entrypoint-initdb.d/init.sql
+++ docker-entrypoint-initdb.d/init.sql
@@ -1,6 +1,5 @@
 create database control_db;
 use control_db;
-CREATE USER control_user;
-alter user control_user ENCRYPTED password 'passw0rd';
-CREATE DATABASE control_user;
-GRANT ALL PRIVILEGES ON DATABASE control_user TO control_user;
+CREATE USER 'control_user'@'%' IDENTIFIED BY 'passw0rd';
+GRANT ALL PRIVILEGES ON *.* TO 'control_user'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
