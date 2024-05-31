462c92b5795901c2f973d114e8eaa4f6917cc4bf
felipe bevi <felipe@9ideias.com>
2024-05-31 15:06:26 -0300

    fixes

--- app/app.py
+++ app/app.py
@@ -318,7 +318,10 @@ def getUsers(type,_json=False):
     pag = request.args.get('pag', 1, type=int)
     qtd = request.args.get('qtd', 5, type=int)
     
-    users_pag = database.get_by_paginated(Users, 'id', page=pag, per_page=qtd, orderby=True)
+    if type==-1:
+        users_pag = database.get_by(Users, 'type', 0, True)
+    else:
+        users_pag = database.get_by_paginated(Users, 'id', page=pag, per_page=qtd, orderby=True)
     
     all_users = []
     for user in users_pag['items']:
