93899259799529ba84c861228ffa648b018ff25c
felipe bevi <felipe@9ideias.com>
2024-05-31 16:08:40 -0300

    fixes

--- app/app.py
+++ app/app.py
@@ -320,11 +320,19 @@ def getUsers(type,_json=False):
     
     if type=='-1':
         users_pag = database.get_by(Users, 'type', 0, 'name')
+        users_pag['total'] = 0
+        users_pag['pages'] = 0
+        users_pag['page'] = 0
+        users_pag['has_prev'] = 0
+        users_pag['has_next'] = 0
+        users_pag['prev_num'] = 0
+        users_pag['next_num'] = 0
     else:
         users_pag = database.get_by_paginated(Users, 'name', page=pag, per_page=qtd)
+        users_pag = ['items']
     
     all_users = []
-    for user in users_pag['items']:
+    for user in users_pag:
         new_user = {
             "id": user.id,
             "name": user.name,
