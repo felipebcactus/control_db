b10c2348fbfe7204c370c9a0568ee9ffc8342d80
felipe bevi <felipe@9ideias.com>
2024-06-20 11:19:36 -0300

    new features

--- app/app.py
+++ app/app.py
@@ -1117,6 +1117,13 @@ def removeSession(user_id_logged, _data_received=None):
     return json.dumps(results), 200
 
 
+@app.route('/removeRequestSession/<id_session>', methods=['POST'])
+@login_required
+def removeRequestSession(id_session):                    
+    database.edit_instance(Sessions, id_session, password=None, status=2, approve_date=None, approver=None)    
+    return json.dumps({}), 200
+
+
 @app.route('/getUserHosts/<user_id>', methods=['GET'])
 @login_required
 def getUserHosts(user_id, _json=False):
